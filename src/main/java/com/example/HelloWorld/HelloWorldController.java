a9d6caecaaa5789c8bec48fb63677bad56ceb4b4
jrn18 <jrn18@my.fsu.edu>
2021-02-01 10:26:46 -0500

    Init. Hello World Spring Boot Application

---
+++ src/main/java/com/example/HelloWorld/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.HelloWorld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/name")
+    public String index(){
+        return "Justin Nahorny";
+    }
+    @RequestMapping("/")
+    public String index1(){
+        return "Hello World!";
+    }
+}
